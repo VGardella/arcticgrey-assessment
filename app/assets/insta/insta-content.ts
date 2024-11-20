e3aaf267f7439c7c36497c671d33149803bcf914
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-11-20 15:18:59 -0300

    fix images extensions

--- app/assets/insta/insta-content.ts
+++ app/assets/insta/insta-content.ts
@@ -6,46 +6,46 @@ export type InstaImagesType = {
 const instaImagesSuperior: InstaImagesType[] = [
   {
     id: 'sup1',
-    imgUrl: '/app/assets/insta/insta-sup1.jfif',
+    imgUrl: '/app/assets/insta/insta-sup1.jpg',
   },
   {
     id: 'sup2',
-    imgUrl: '/app/assets/insta/insta-sup2.jfif',
+    imgUrl: '/app/assets/insta/insta-sup2.jpg',
   },
   {
     id: 'sup3',
-    imgUrl: '/app/assets/insta/insta-sup3.jfif',
+    imgUrl: '/app/assets/insta/insta-sup3.jpg',
   },
   {
     id: 'sup4',
-    imgUrl: '/app/assets/insta/insta-sup4.jfif',
+    imgUrl: '/app/assets/insta/insta-sup4.jpg',
   },
 ];
 
 const instaImagesInferior: InstaImagesType[] = [
   {
     id: 'inf1',
-    imgUrl: '/app/assets/insta/insta-inf1.jfif',
+    imgUrl: '/app/assets/insta/insta-inf1.jpg',
   },
   {
     id: 'inf2',
-    imgUrl: '/app/assets/insta/insta-inf2.jfif',
+    imgUrl: '/app/assets/insta/insta-inf2.jpg',
   },
   {
     id: 'inf3',
-    imgUrl: '/app/assets/insta/insta-inf3.jfif',
+    imgUrl: '/app/assets/insta/insta-inf3.jpg',
   },
   {
     id: 'inf4',
-    imgUrl: '/app/assets/insta/insta-inf4.jfif',
+    imgUrl: '/app/assets/insta/insta-inf4.jpg',
   },
   {
     id: 'inf5',
-    imgUrl: '/app/assets/insta/insta-inf5.jfif',
+    imgUrl: '/app/assets/insta/insta-inf5.jpg',
   },
   {
     id: 'inf6',
-    imgUrl: '/app/assets/insta/insta-inf6.jfif',
+    imgUrl: '/app/assets/insta/insta-inf6.jpg',
   },
 ];
 
