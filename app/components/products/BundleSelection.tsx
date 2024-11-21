b90ef8a92bf39b91be2d0e18be9a5ec4a313d310
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-11-20 23:25:13 -0300

    extract component

--- app/components/products/BundleSelection.tsx
+++ app/components/products/BundleSelection.tsx
@@ -3,6 +3,7 @@ import type {ProductNode} from '~/routes/_index';
 import {NavBar} from '../NavBar';
 import {ProductImage} from '../ui/products/ProductImage';
 import {AddToCartButton} from '../cart/AddToCartButton';
+import {TagsProductCard} from '../ui/products/TagsProductCard';
 
 export function BundlesSelection({
   productList,
@@ -28,105 +29,20 @@ export function BundlesSelection({
             <h4 className="underline text-lg">View All Bundles</h4>
             <div className="flex gap-2">
               <button className="w-10 h-10 bg-[#F5F5F5] border border-[#1B1F231A] rounded cursor-pointer">
-                <img
-                  className="h-[10px] m-auto"
-                  src="/../assets/arrow-l.png"
-                />
+                <img className="h-[10px] m-auto" src="/../assets/arrow-l.png" />
               </button>
               <button className="w-10 h-10 bg-[#F5F5F5] border border-[#1B1F231A] rounded cursor-pointer">
-                <img
-                  className="h-[10px] m-auto"
-                  src="/../assets/arrow-r.png"
-                />
+                <img className="h-[10px] m-auto" src="/../assets/arrow-r.png" />
               </button>
             </div>
           </div>
         </div>
         <div className="flex gap-5">
           {bundles.map((item) => {
-            return (
-              <div className="flex flex-col rounded-lg bg-[#F6F6F5] w-[365px] h-[505px]">
-                <div className="flex justify-center items-center">
-                  <div className="w-[365px] h-[365px] p-10">
-                    <ProductImage image={item.images.edges[0].node} />
-                  </div>
-                </div>
-                <div className="flex flex-col gap-4 mx-5 mb-5 mt-auto w-[325px] h-[110px]">
-                  <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 w-full">
-                    {(item.metafields &&
-                    item.metafields.some((field) => field !== null)
-                      ? item.metafields
-                      : FALLBACK_METAFIELDS
-                    ).map((field) => {
-                      return (
-                        <div
-                          className="px-[10px] py-[5px] bg-white rounded text-[10px] leading-3 whitespace-nowrap w-min"
-                          key={field.id}
-                        >
-                          &#11044; {field.value}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div className="flex flex-col w-[325px] gap-2 justify-between">
-                    <div className="flex flex-col gap-[5px]">
-                      <h3 className="font-medium leading-[19px]">
-                        {item.title}
-                      </h3>
-                      <p className="text-[13px] leading-[15px]">
-                        {item.tags.join(', ')}
-                      </p>
-                    </div>
-                    <div className="flex justify-between items-center">
-                      <div className="flex">
-                        {Array.from({length: 5}).map((_, index) => (
-                          <div key={index}>
-                            <img
-                              className="w-3 h-3"
-                              src="/../assets/black-star.png"
-                            />
-                          </div>
-                        ))}
-                      </div>
-                      <div className="px-[15px] py-[5px] bg-[#1B1F23] text-white text-[13px] leading-[15px] rounded cursor-pointer">
-                        <AddToCartButton
-                          disabled={!item.availableForSale}
-                          onClick={() => {
-                            open('cart');
-                          }}
-                          lines={
-                            item
-                              ? [
-                                  {
-                                    merchandiseId: item.id,
-                                    quantity: 1,
-                                  },
-                                ]
-                              : []
-                          }
-                        >
-                          {item?.availableForSale ? `Add to Cart` : 'Sold out'}
-                        </AddToCartButton>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
+            return <TagsProductCard key={item.id} product={item} />;
           })}
         </div>
       </div>
     </div>
   );
 }
-
-const FALLBACK_METAFIELDS = [
-  {
-    id: 'dietary1',
-    value: 'Gluten Free',
-  },
-  {
-    id: 'dietary2',
-    value: 'GMO Free',
-  },
-];
---
+++ app/components/ui/products/TagsProductCard.tsx
@@ -0,0 +1,84 @@
+import {AddToCartButton} from '~/components/cart/AddToCartButton';
+import {ProductImage} from './ProductImage';
+import type {ProductNode} from '~/routes/_index';
+
+export function TagsProductCard({product}: {product: ProductNode}) {
+  return (
+    <div
+      key={product.id}
+      className="flex flex-col rounded-lg bg-[#F6F6F5] w-[365px] h-[505px]"
+    >
+      <div className="flex justify-center items-center">
+        <div className="w-[365px] h-[365px] p-10">
+          <ProductImage image={product.images.edges[0].node} />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 mx-5 mb-5 mt-auto w-[325px] h-[110px]">
+        <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 w-full">
+          {(product.metafields &&
+          product.metafields.some((field) => field !== null)
+            ? product.metafields
+            : FALLBACK_METAFIELDS
+          ).map((field) => {
+            return (
+              <div
+                className="px-[10px] py-[5px] bg-white rounded text-[10px] leading-3 whitespace-nowrap w-min"
+                key={field.id}
+              >
+                &#11044; {field.value}
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex flex-col w-[325px] gap-2 justify-between">
+          <div className="flex flex-col gap-[5px]">
+            <h3 className="font-medium leading-[19px]">{product.title}</h3>
+            <p className="text-[13px] leading-[15px]">
+              {product.tags.join(', ')}
+            </p>
+          </div>
+          <div className="flex justify-between items-center">
+            <div className="flex">
+              {Array.from({length: 5}).map((_, index) => (
+                <div key={index}>
+                  <img className="w-3 h-3" src="/../assets/black-star.png" />
+                </div>
+              ))}
+            </div>
+            <div className="px-[15px] py-[5px] bg-[#1B1F23] text-white text-[13px] leading-[15px] rounded cursor-pointer">
+              <AddToCartButton
+                disabled={!product.availableForSale}
+                onClick={() => {
+                  open('cart');
+                }}
+                lines={
+                  product
+                    ? [
+                        {
+                          merchandiseId: product.id,
+                          quantity: 1,
+                        },
+                      ]
+                    : []
+                }
+              >
+                {product?.availableForSale ? `Add to Cart` : 'Sold out'}
+              </AddToCartButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const FALLBACK_METAFIELDS = [
+  {
+    id: 'dietary1',
+    value: 'Gluten Free',
+  },
+  {
+    id: 'dietary2',
+    value: 'GMO Free',
+  },
+];
