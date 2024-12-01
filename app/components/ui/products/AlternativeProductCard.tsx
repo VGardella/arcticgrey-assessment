83be38a26a7d3b5a26f1b2d4479dd49110f948b1
Victoria Gardella <v.gardella.ruiz@gmail.com>
2024-11-30 21:20:22 -0300

    refactor input type and property management

--- app/components/ui/products/AlternativeProductCard.tsx
+++ app/components/ui/products/AlternativeProductCard.tsx
@@ -1,13 +1,16 @@
-import type {ProductNode} from '~/routes/_index';
 import {ProductImage} from './ProductImage';
 import {AddToCartButton} from '../../cart/AddToCartButton';
+import { ProductFragment } from 'storefrontapi.generated';
+import { useAside } from '~/components/aside/hooks/useAside';
 
-export function AlternativeCard({product}: {product: ProductNode}) {
+export function AlternativeCard({product}: {product: ProductFragment}) {
+  const {open} = useAside();
+
   return (
     <div className="flex flex-col rounded-lg bg-white w-[365px] h-[505px] items-center">
       <div className="flex justify-center items-center">
         <div className="w-[365px] h-[365px] p-10">
-          <ProductImage image={product.images.edges[0].node} />
+          <ProductImage image={product.variants.nodes[0].image} />
         </div>
       </div>
       <div className="flex flex-col gap-[10px] justify-center -mt-4">
@@ -27,7 +30,7 @@ export function AlternativeCard({product}: {product: ProductNode}) {
                 htmlFor="one-time"
               >
                 One-Time Purchase <br />${' '}
-                {product.priceRange.maxVariantPrice.amount}
+                {product.variants.nodes[0].price.amount}
               </label>
             </div>
             <div className="flex bg-[#F6F6F5] rounded-md border border-[#EEEEEE] w-full gap-2 p-2">
@@ -44,30 +47,30 @@ export function AlternativeCard({product}: {product: ProductNode}) {
                 htmlFor="subscribe"
               >
                 Subscribe & Save <br />${' '}
-                {product.priceRange.maxVariantPrice.amount}
+                {product.variants.nodes[0].price.amount}
               </label>
             </div>
           </form>
         </div>
         <div className="h-[45px] w-[325px] py-[14px] rounded-lg bg-[#1B1F23] text-white font-medium text-[14px] text-center leading-[17px] cursor-pointer">
           <AddToCartButton
-            disabled={!product.availableForSale}
+            disabled={!product.variants.nodes[0].availableForSale}
             onClick={() => {
               open('cart');
             }}
             lines={
-              product
+              product.variants.nodes[0]
                 ? [
                     {
-                      merchandiseId: product.id,
+                      merchandiseId: product.variants.nodes[0].id,
                       quantity: 1,
                     },
                   ]
                 : []
             }
           >
-            {product?.availableForSale
-              ? `Add to Cart - $${product.priceRange.maxVariantPrice.amount}`
+            {product?.variants.nodes[0].availableForSale
+              ? `Add to Cart - $${product.variants.nodes[0].price.amount}`
               : 'Sold out'}
           </AddToCartButton>
         </div>
